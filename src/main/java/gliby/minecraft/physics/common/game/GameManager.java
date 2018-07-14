9c010773aa0d2d06629dbaa415324182f3af7c86
gdev <130885-chemicalheads0@users.noreply.gitlab.com>
2018-07-14 03:09:29 +0300

    added item recipes

--- src/main/java/gliby/minecraft/physics/common/game/GameManager.java
+++ src/main/java/gliby/minecraft/physics/common/game/GameManager.java
@@ -11,7 +11,10 @@ import gliby.minecraft.physics.common.game.items.toolgun.actions.ToolGunAttractA
 import gliby.minecraft.physics.common.game.items.toolgun.actions.ToolGunChangeGravityAction;
 import gliby.minecraft.physics.common.game.items.toolgun.actions.ToolGunRemoveAction;
 import gliby.minecraft.physics.common.game.items.toolgun.actions.ToolGunReviveAction;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -24,7 +27,7 @@ public class GameManager {
 	}
 
 	private ToolGunActionRegistry toolGunRegistry;
-	
+
 	private static Item itemRigidBodySpawner;
 	public RawItem itemPhysicsGun, itemToolgun;
 
@@ -41,6 +44,16 @@ public class GameManager {
 		toolGunRegistry.registerAction(new ToolGunAttractAction(), Physics.ID);
 		toolGunRegistry.registerAction(new ToolGunChangeGravityAction(), Physics.ID);
 		toolGunRegistry.registerAction(new ToolGunRemoveAction(), Physics.ID);
+
+		GameRegistry.addRecipe(new ItemStack(itemPhysicsGun), "AAA", "BCB", "AAA", 'A', Blocks.obsidian, 'B',
+				Items.redstone, 'C', Items.diamond);
+
+		GameRegistry.addRecipe(new ItemStack(itemToolgun), "AAA", "CBC", "AAA", 'A', Items.redstone, 'B',
+				itemPhysicsGun, 'C', Items.diamond);
+		/*
+		 * GameRegistry.addRecipe(new ItemStack(Items.dye, 2, 15), "AB ", "AAC", "A  ",
+		 * 'A', Items.cookie, 'B', Blocks.dirt, 'C', new ItemStack(Items.dye, 1, 1));
+		 */
 		// toolGunRegistry.registerAction(new ToolGunMotorAction(),
 		// Physics.MOD_ID);
 	}
