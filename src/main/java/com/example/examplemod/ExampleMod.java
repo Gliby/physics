c6f1311478fc4379ac95598f9f98f2c72f4474ae
gdev <130885-chemicalheads0@users.noreply.gitlab.com>
2019-05-01 04:43:11 +0300

    basic gradle setup

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -7,12 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = "{@version:mod}", acceptedMinecraftVersions = "{@version:mc}")
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "glibysphysics";
+    public static final String NAME = "Gliby's Physics";
 
     private static Logger logger;
 
